0e81831a19b93d8c0413ea96fc4c4a67b22fe207
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-01-02 14:37:46 +0100

    Add Junit 5 test framework

---
+++ src/test/java/actors/ActorContextTest.java
@@ -0,0 +1,13 @@
+package actors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ActorContextTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
